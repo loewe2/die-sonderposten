02bebe1d55e10b059c7b1c96e9732edb93472ffa
TIm97Egl <timengel11@web.de>
2022-07-08 02:08:15 +0200

    Abgabe für xgboost

--- predict.py
+++ predict.py
@@ -9,7 +9,6 @@ Skript testet das vortrainierte Modell
 
 import csv
 import scipy.io as sio
-import matplotlib.pyplot as plt
 import numpy as np
 from ecgdetectors import Detectors
 import os
@@ -24,7 +23,11 @@ import scipy.signal as siglib
 from keras.models import Sequential
 import tensorflow.keras as keras
 import tensorflow as tf
-
+import neurokit2 as nk
+import utilz
+import pandas as pd
+import xgboost as xgb
+import warnings
 
 ###Signatur der Methode (Parameter und Anzahl return-Werte) darf nicht verändert werden
 def predict_labels(ecg_leads : List[np.ndarray], fs : float, ecg_names : List[str], model_name : str='tree_model.sav',is_binary_classifier : bool=False) -> List[Tuple[str,str]]:
@@ -82,7 +85,7 @@ def predict_labels(ecg_leads : List[np.ndarray], fs : float, ecg_names : List[st
         predictions = list(zip(ecg_names, predicted_label_return))
 
 
-    elif(model_name == 'gs_grabost.sav'):
+    elif(model_name == 'tree_model.sav'):
         hrz = hrv.HRV(fs)
         detectors = Detectors(fs)                                 # Initialisierung des QRS-Detektors
         sdnn_array = np.array([])                                # Initialisierung der Feature-Arrays
@@ -143,6 +146,33 @@ def predict_labels(ecg_leads : List[np.ndarray], fs : float, ecg_names : List[st
             else:
                 result.append('A')
         predictions = list(zip(ecg_names, result))
+    
+    elif(model_name == 'xgboost_abgabe.json'):
+        warnings.filterwarnings('ignore')
+        with open('dftemplate.pkl','rb') as target:
+            dftemplate = pickle.load(target)
+        model = xgb.XGBClassifier()
+        model.load_model(model_name)
+        base_dataframe = pd.read_pickle('./base_dataframe.pkl')
+        results = []
+        for ecg_lead in ecg_leads:
+            signal = ecg_lead
+            try:
+                signals, info = nk.ecg_process(signal, sampling_rate=fs, method='neurokit')
+                analyzed = nk.ecg_analyze(signals, sampling_rate=fs)
+                own = utilz.ownFeatures(signals)
+                analyzed = pd.concat([analyzed,own], axis=1)
+                analyzed.replace([np.inf, -np.inf], np.nan, inplace=True)
+                analyzed = pd.concat([dftemplate,analyzed], axis=0)
+                analyzed = analyzed[dftemplate.columns.to_list()].iloc[1].to_frame().T
+                prediction = model.predict(analyzed)
+                if(prediction == 0):
+                    results.append('N')
+                else:
+                    results.append('A')
+            except:
+                results.append('N')
+        predictions = list(zip(ecg_names, results))
     #     #print(predictions)
 
 #------------------------------------------------------------------------------    
